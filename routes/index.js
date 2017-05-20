6c246994baaf6856766ae85f0c466c1ead83154f
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-05-20 20:49:36 +0200

    added seeds

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var Message = require("../models/message");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +8,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.get("/dashboard", (req, res, next)=>{
-  res.render("dashboard");
+  Message.find((err, messages)=>{
+    if(err) next(err);
+    res.render("dashboard", {messages});
+  });
+
 });
 
 router.get('/signup_b', function(req, res, next) {
