570350327f855e8d4c82f20e93424dfc50d1108e
Andreas Karpasitis <karpas@Andreass-MacBook-Pro.local>
2017-05-20 16:09:56 +0200

    index done signup done

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(expressLayouts);
 app.set('layout', 'layouts/main-layout');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 
 
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'FriendStreet' });
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('users/signup', {account: req.body.account});
+});
+
 module.exports = router;
