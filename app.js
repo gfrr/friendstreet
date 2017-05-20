90d5589f5c9dabd90251b2445ffadb7650f08c38
Andreas Karpasitis <karpas@Andreass-MacBook-Pro.local>
2017-05-20 14:46:57 +0200

    added css links working on layout

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressLayouts = require("express-ejs-layouts");
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+app.use(expressLayouts);
+app.set('layout', 'layouts/main-layout');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
