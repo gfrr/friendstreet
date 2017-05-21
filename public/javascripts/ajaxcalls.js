0c5dcc3d9659400e000d9b15025a5ae5d41be82a
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-05-21 09:23:11 +0200

    added static voting

--- public/javascripts/ajaxcalls.js
+++ public/javascripts/ajaxcalls.js
@@ -1,5 +1,22 @@
 const dbAPI = new APIHandler();
 
 $(document).ready(()=>{
-  dbAPI.getFeedbackInfo($("#itemId").val(), feedInfo);
+  // dbAPI.getFullList("messages");
+  $(".main").on("click", ".test", (event)=>{
+    console.log(event);
+    console.log(event.currentTarget.value);
+    console.log(event.currentTarget.id);
+    if(event.currentTarget.id[0]=="+"){
+      dbAPI.updateOneRegister("messages", event.currentTarget.id.slice(1), {score: Number(event.currentTarget.value) +1 });
+    }
+    else{
+      dbAPI.updateOneRegister("messages", event.currentTarget.id.slice(1), {score: Number(event.currentTarget.value) -1 });
+    }
+  });
+
 });
+
+
+function vote(sign, messageid){
+
+}
