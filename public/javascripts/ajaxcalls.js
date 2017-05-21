2364d97827ced3d0bdfcdea695b14c7fce0df8fd
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-05-21 09:52:44 +0200

    styling

--- public/javascripts/ajaxcalls.js
+++ public/javascripts/ajaxcalls.js
@@ -6,11 +6,18 @@ $(document).ready(()=>{
     console.log(event);
     console.log(event.currentTarget.value);
     console.log(event.currentTarget.id);
+
     if(event.currentTarget.id[0]=="+"){
       dbAPI.updateOneRegister("messages", event.currentTarget.id.slice(1), {score: Number(event.currentTarget.value) +1 });
+      event.currentTarget.parentElement.children[1].innerHTML = Number(event.currentTarget.value)+1;
+      $(event.currentTarget.parentElement.children[0]).hide();
+      $(event.currentTarget.parentElement.children[2]).hide();
     }
     else{
       dbAPI.updateOneRegister("messages", event.currentTarget.id.slice(1), {score: Number(event.currentTarget.value) -1 });
+      event.currentTarget.parentElement.children[1].innerHTML = Number(event.currentTarget.value)-1;
+      $(event.currentTarget.parentElement.children[0]).hide();
+      $(event.currentTarget.parentElement.children[2]).hide();
     }
   });
 
